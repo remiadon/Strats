b965dd47d1d30da89da6672d7e7d94a5f18438dc
Rémi Adon <remi.adon@protonmail.com>
2026-03-09 12:34:18 +0100

    g7 countries in esg related data

--- extract/eu_uncertainty.py
+++ extract/eu_uncertainty.py
@@ -6,6 +6,7 @@ import time
 from . import kw, dump
 
 #countries = ['France', 'UK', 'Germany', 'Italy', 'Spain', 'Russia', 'Netherlands', 'Sweden', 'Poland', 'Belgium']
+g7 = ['US', 'UK', 'France', 'Germany', 'Italy', 'Japan', 'Canada']
 
 eupu = pl.read_excel(
     'https://policyuncertainty.com/media/Europe_Policy_Uncertainty_Data.xlsx',
@@ -14,14 +15,15 @@ eupu = pl.read_excel(
 eupu = eupu.filter(pl.concat_list(pl.exclude('Year', 'Month')).list.drop_nulls().list.len() > 4)
 eupu = eupu.select(
     pl.date('Year', 'Month', 1).alias('Date'),
-    pl.exclude('Year', 'Month').name.suffix('_newspaper_policy_uncertainty'),
+    pl.exclude('Year', 'Month').name.suffix('_policy_uncertainty'),
 )
 esg = pl.read_excel(
     'https://policyuncertainty.com/media/ESGUI_Indexes.xlsx', read_options=dict(header_row=2),
     infer_schema_length=10_000,
+    columns=['Date', 'Global_GDP_Weighted', 'Global_Equal_Weighted'] + g7,
 ).select(
     pl.col.Date.str.to_date('%Y %b', strict=False),
-    pl.exclude('^Global.*$').name.suffix('_esg_policy_uncertainty'),
+    pl.exclude('Date').name.suffix('_esg_policy_uncertainty'),
 ).drop_nulls(subset='Date')
 
 df = reduce(lambda x, y: x.join(y, on='Date', how='full', validate='1:1', coalesce=True), [eupu, esg]).sort('Date')
